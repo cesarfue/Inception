0778f965a05b74470459f5ccaa64eca47405d34b
cefue <cesar.fuentes@outlook.fr>
2024-08-14 07:29:23 +0200

    .

---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -f ./wp-config.php ]; then
+  sleep 10
+  wp config create --allow-root \
+    --dbname=$SQL_DATABASE \
+    --dbuser=$SQL_USER \
+    --dbpass=$SQL_PASSWORD \
+    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+fi
+
+exec "$@"
