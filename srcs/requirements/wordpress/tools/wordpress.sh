1411a3cf1d1b0d636ee9b38f777ab3170ac7d0df
cefue <cesar.fuentes@outlook.fr>
2024-08-19 16:30:28 +0200

    .

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-while [ ! -f ./wp-config.php ]; do
-  sleep 1
-done
+# if [ -f ./wp-config.php ]; then
+# echo "wordpress already downloaded"
+# else
 
-wp config create --allow-root \
-  --dbname=$SQL_DATABASE \
-  --dbuser=$SQL_USER \
-  --dbpass=$SQL_PASSWORD \
-  --dbhost=mariadb:3306 --path='/var/www/wordpress'
+wget http://wordpress.org/latest.tar.gz
+tar xfz latest.tar.gz
+mv wordpress/* .
+rm -rf latest.tar.gz
+rm -rf wordpress
+
+sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+cp wp-config-sample.php wp-config.php
+
+# fi
 
 exec "$@"
