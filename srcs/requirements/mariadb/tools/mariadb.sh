358313288bce3e01ca3f6a42e8154f26e2fb5d21
cefue <cesar.fuentes@outlook.fr>
2024-08-20 16:34:03 +0200

    .

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
-mysql_install_db
+if ! mysql -e 'use mysql; select User from user;' >/dev/null 2>&1; then
+  mysql_install_db
+fi
 
 mysqld_safe &
 
 while ! mysqladmin ping --silent; do
-  sleep 1
+  sleep 10
 done
-
+echo prout
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
