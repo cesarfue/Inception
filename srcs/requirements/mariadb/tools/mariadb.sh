257fc94e72c0225a4737e7e7d6c71acc2c4cc0a4
cefue <cesar.fuentes@outlook.fr>
2024-08-22 18:57:50 +0200

    fixed mariadb .sh script : 2 mysql -uroot cmds were run too quickly

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-mysql_safe &
-
 /etc/init.d/mysql start
 
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+mysql -uroot -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD'; \
+  FLUSH PRIVILEGES;"
 
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE; GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-
-mysql -uroot -p$SQL_ROOT_PASSWORD $SQL_DATABASE </usr/local/bin/wordpress.sql
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE; \
+  GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; \
+  FLUSH PRIVILEGES;"
 
 /etc/init.d/mysql stop
 
