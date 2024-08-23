180f23daeb65a501fec746130a9ed9fbcfd5994d
cefue <cesar.fuentes@outlook.fr>
2024-08-23 11:55:31 +0200

    changed mariadb.sh for more clarity

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-/etc/init.d/mysql start
+service mysql start
 
 mysql -uroot -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD'; \
   FLUSH PRIVILEGES;"
-
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE; \
-  GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; \
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+mysql -root -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+sleep 1
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; \
   FLUSH PRIVILEGES;"
 
-/etc/init.d/mysql stop
+service mysql stop
 
 exec "$@"
